56c68e6ef1758a5f41fd54df3467e2b188b3757a
TJ <tonyjsouthern@gmail.com>
2019-07-10 14:18:28 -0700

    Initial commit with ui layout

---
+++ render/src/app.js
@@ -0,0 +1,30 @@
+import { inject } from 'aurelia-framework';
+import { Router } from 'aurelia-router';
+
+@inject(Router)
+export class App {
+
+  configureRouter(config, router) {
+    this.router = router;
+    config.title = 'flagstaff';
+    config.map([
+      { route: '',          name: 'select',     moduleId: 'select',    title: 'select'     },
+      { route: 'editor',  name: 'editor',  moduleId: 'editor', title: 'editor'  }
+ ]);
+}
+
+
+  constructor(router){
+    this.router = router;
+    this.expanded = false;
+  }
+
+  actiavte(){
+
+  }
+
+  alert(){
+    this.shannon = this.message;
+  }
+
+}
